41d94e7d9172edb848606f926082a29107befa00
John O’Brien <johnobrien8642@gmail.com>
2021-06-17 15:19:11 -0700

    try different static

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ cronPostNotesHeat.start()
 cronUserPostingHeat.start()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/public'))
+  app.use(express.static('client'))
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
   })
