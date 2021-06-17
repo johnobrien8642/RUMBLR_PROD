7cc8f51359253b13cebbdbd47e1cdfee9a71420c
John O’Brien <johnobrien8642@gmail.com>
2021-06-17 14:06:01 -0700

    fix create-react-app webpack version issue

--- config/keys.js
+++ config/keys.js
@@ -7,6 +7,5 @@ export default {
   secretOrKey: process.env.SECRET_OR_KEY,
   emailAuthToken: process.env.EMAIL_AUTH_TOKEN,
   hostEmail: process.env.HOST_EMAIL,
-  hostPassword: process.env.HOST_PASSWORD,
-  mongoURL: process.env.MONGO_URL
+  hostPassword: process.env.HOST_PASSWORD
 }
