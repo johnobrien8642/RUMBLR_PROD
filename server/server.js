b5f71f2f10a61aee6c35efaa753d805669a15897
John O’Brien <johnobrien8642@gmail.com>
2021-06-17 15:37:15 -0700

    try without app.use app.get static client

--- server/server.js
+++ server/server.js
@@ -32,12 +32,12 @@ cronTagPostHeat.start()
 cronPostNotesHeat.start()
 cronUserPostingHeat.start()
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client'))
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client'))
+//   app.get('/*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
+//   })
+// }
   
 app.use(express.json())
 app.use('/api/posts', posts);
