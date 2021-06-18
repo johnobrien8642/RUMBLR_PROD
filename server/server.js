9cc21391006cd727e2ecb0c51b4886ea544e2c40
John O’Brien <johnobrien8642@gmail.com>
2021-06-17 17:22:53 -0700

    add console log for testing

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const { cronTagFollowerHeat,
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('made it to serving client')
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
