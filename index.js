2bfdca39b551d087e92ed24ee972b949e9d58d95
John O’Brien <johnobrien8642@gmail.com>
2021-06-18 15:03:23 -0700

    re-install npm

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-import app from './server/server.js';
-import mongoose from 'mongoose';
-import keys from './config/keys/keys.js';
+import app from './server/server.js'
 
 const port = process.env.PORT || 5000
 
-mongoose
-  .connect(keys.mongoURL, { 
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => {
-    console.log('Connected to MongoDB successfully')
-    app.listen({ port: port }, () => console.log(`Server listening on port ${port}`))
-  }).catch(err => console.log(err))
+
+app.listen({ port: port }, () => console.log(`Server listening on port ${port}`))
 
 
