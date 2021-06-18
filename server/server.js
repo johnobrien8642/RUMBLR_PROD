fe2ed64e03a02afbbc93f822a090d4e46d45bd85
John O’Brien <johnobrien8642@gmail.com>
2021-06-18 02:10:11 -0700

    try without conditional server.js

--- server/server.js
+++ server/server.js
@@ -17,13 +17,13 @@ const { cronTagFollowerHeat,
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   console.log('made it to serving client')
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-}
+// }
 
 mongoose
   .connect(keys.mongoURL, { 
