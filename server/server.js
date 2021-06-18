8ad3a0d19fb5d7444cf385777042950f773a5d58
John O’Brien <johnobrien8642@gmail.com>
2021-06-17 17:46:01 -0700

    try more specific express.static()

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 if (process.env.NODE_ENV === 'production') {
   console.log('made it to serving client')
-  app.use(express.static('client/build'))
+  app.use(express.static(path.resolve(__dirname, 'client', 'build', 'index.html')))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
