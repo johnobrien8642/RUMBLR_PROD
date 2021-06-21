b8f24cc0597e59449c48f5a52e18270f9918ac3f
John O’Brien <johnobrien8642@gmail.com>
2021-06-20 18:41:59 -0700

    try console log outside of if statement

--- server/server.js
+++ server/server.js
@@ -30,8 +30,8 @@ mongoose
 .then(() => console.log('Connected to MongoDB successfully'))
 .catch(err => console.log(err))
 
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
-  console.log(process.env.NODE_ENV)
   app.use(express.static('client/build'))
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
