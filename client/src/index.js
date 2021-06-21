f6cd0d8bc142227a76388965e7472cd886120346
John O’Brien <johnobrien8642@gmail.com>
2021-06-20 22:41:03 -0700

    try new frontend build

--- client/src/index.js
+++ client/src/index.js
@@ -29,7 +29,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const httpLink = new HttpLink({
-  uri: `http://localhost:8080/graphql`,
+  uri: `http://localhost:${port}/graphql`,
 });
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
