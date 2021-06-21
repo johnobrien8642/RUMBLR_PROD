ca5b0bd59cac3be6563b3463a3175c15f4419b7f
John O’Brien <johnobrien8642@gmail.com>
2021-06-20 21:32:02 -0700

    add back get for build

--- server/server.js
+++ server/server.js
@@ -31,10 +31,10 @@ mongoose
 .then(() => console.log('Connected to MongoDB successfully'))
 .catch(err => console.log(err))
 
-// app.use(express.static('client/build'))
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-// })
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 
 cronTagFollowerHeat.start()
