08bd61f5b97cfb87ff161b2d2263ffed6d77e7cb
John O’Brien <johnobrien8642@gmail.com>
2021-06-20 20:46:32 -0700

    this doesn't matter

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,13 @@
 // import keys_prod from './keys_prod'
-import keys_dev from './keys_dev.js'
-export default keys_dev;
+// import keys_dev from './keys_dev.js'
+// export default keys_dev;
 
 // export default process.env.NODE_ENV === 'development' ? keys_dev : keys_prod;
 
-// export default {
-//   secretOrKey: process.env.SECRET_OR_KEY,
-//   emailAuthToken: process.env.EMAIL_AUTH_TOKEN,
-//   hostEmail: process.env.HOST_EMAIL,
-//   hostPassword: process.env.HOST_PASSWORD,
-//   mongoURL: process.env.MONGO_URL
-// }
+export default {
+  secretOrKey: process.env.SECRET_OR_KEY,
+  emailAuthToken: process.env.EMAIL_AUTH_TOKEN,
+  hostEmail: process.env.HOST_EMAIL,
+  hostPassword: process.env.HOST_PASSWORD,
+  mongoURL: process.env.MONGO_URL
+}
--- server/config/keys.js
+++ server/config/keys.js
@@ -1,13 +1,13 @@
 // import keys_prod from './keys_prod.js'
-import keys_dev from './keys_dev.js'
-export default keys_dev;
+// import keys_dev from './keys_dev.js'
+// export default keys_dev;
 
 // export default process.env.NODE_ENV === 'development' ? keys_dev : keys_prod;
 
-// export default {
-//   localIP: process.env.LOCAL_IP,
-//   accessKeyId: process.env.ACCESS_KEY_ID,
-//   secretAccessKey: process.env.SECRET_ACCESS_KEY,
-//   bucket: process.env.BUCKET,
-//   mongoURL: process.env.MONGO_URL
-// }
+export default {
+  localIP: process.env.LOCAL_IP,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  bucket: process.env.BUCKET,
+  mongoURL: process.env.MONGO_URL
+}
