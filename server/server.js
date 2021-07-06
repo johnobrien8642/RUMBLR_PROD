d3f7d4c48237aee1eb445854b4017b6b4bb0dc96
John O’Brien <johnobrien8642@gmail.com>
2021-07-06 09:53:00 -0700

    try NODE_ENV server if statement again

--- server/server.js
+++ server/server.js
@@ -30,10 +30,12 @@ mongoose
 .then(() => console.log('Connected to MongoDB successfully'))
 .catch(err => console.log(err))
 
-app.use(express.static('client/build'))
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 
 cronTagFollowerHeat.start()
