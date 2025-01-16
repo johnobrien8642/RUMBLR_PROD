118a917f22d7e4caca11e5b4ab7c54ea751954e5
John O'Brien <johnobrien8642@gmail.com>
2025-01-15 18:30:51 -0800

    try downgrade referrer

--- client/src/components/posts/util/functions/post_show_util.js
+++ client/src/components/posts/util/functions/post_show_util.js
@@ -291,7 +291,7 @@ const postBody = (post) => {
       <React.Fragment>
         {
           data.videoLink.url.includes('youtube') &&
-            <iframe width="100%" height="315" src={data.videoLink.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="100%" height="315" src={data.videoLink.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="no-referrer-when-downgrade" allowfullscreen></iframe>
         }
         {
           !data.videoLink.url.includes('youtube') &&
